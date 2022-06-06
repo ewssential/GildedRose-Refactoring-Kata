62f81e2d8ff321562e379651e990542b5ef3a234
Eike Wilke <eike.wilke@mailbox.org>
2022-06-06 14:51:41 +0200

    extract method

--- csharpcore/GildedRose/GildedRose.cs
+++ csharpcore/GildedRose/GildedRose.cs
@@ -9,7 +9,7 @@ namespace GildedRoseKata
         private static readonly string BackstagePassesToATafkal80EtcConcert = "Backstage passes to a TAFKAL80ETC concert";
         private static readonly string SulfurasHandOfRagnaros = "Sulfuras, Hand of Ragnaros";
 
-        private static readonly List<string> IncreaseQuality = new List<string>()
+        private static readonly List<string> IncreaseQualityItems = new List<string>()
             { AgedBrie, BackstagePassesToATafkal80EtcConcert };
         
         public GildedRose(IList<Item> Items)
@@ -24,7 +24,69 @@ namespace GildedRoseKata
                 if (t.Name == SulfurasHandOfRagnaros)
                     continue;
                     
-                if (!IncreaseQuality.Contains(t.Name))
+                if (IncreaseQualityPossible(t))
+                {
+                    IncreaseQuality(t);
+                }
+                else
+                {
+                    if (t.Quality > 0)
+                    {
+                        t.Quality = t.Quality - 1;
+                    } 
+                }
+
+                t.SellIn = t.SellIn - 1;
+
+                if (t.SellIn < 0)
+                {
+                    SellInLessZero(t);
+                }
+            }
+        }
+
+        private static bool IncreaseQualityPossible(Item t)
+        {
+            return IncreaseQualityItems.Contains(t.Name);
+        }
+
+        private static void IncreaseQuality(Item t)
+        {
+            if (t.Quality < 50)
+            {
+                IncreamentQuality(t);
+
+                if (t.Name == BackstagePassesToATafkal80EtcConcert)
+                {
+                    if (t.SellIn < 11)
+                    {
+                        if (t.Quality < 50)
+                        {
+                            IncreamentQuality(t);
+                        }
+                    }
+
+                    if (t.SellIn < 6)
+                    {
+                        if (t.Quality < 50)
+                        {
+                            IncreamentQuality(t);
+                        }
+                    }
+                }
+            }
+        }
+
+        private static int IncreamentQuality(Item t)
+        {
+            return t.Quality == 50 ? 50 : t.Quality = t.Quality + 1;
+        }
+
+        private static void SellInLessZero(Item t)
+        {
+            if (t.Name != AgedBrie)
+            {
+                if (t.Name != BackstagePassesToATafkal80EtcConcert)
                 {
                     if (t.Quality > 0)
                     {
@@ -33,56 +95,14 @@ namespace GildedRoseKata
                 }
                 else
                 {
-                    if (t.Quality < 50)
-                    {
-                        t.Quality = t.Quality + 1;
-
-                        if (t.Name == BackstagePassesToATafkal80EtcConcert)
-                        {
-                            if (t.SellIn < 11)
-                            {
-                                if (t.Quality < 50)
-                                {
-                                    t.Quality = t.Quality + 1;
-                                }
-                            }
-
-                            if (t.SellIn < 6)
-                            {
-                                if (t.Quality < 50)
-                                {
-                                    t.Quality = t.Quality + 1;
-                                }
-                            }
-                        }
-                    }
+                    t.Quality = t.Quality - t.Quality;
                 }
-
-                t.SellIn = t.SellIn - 1;
-
-                if (t.SellIn < 0)
+            }
+            else
+            {
+                if (t.Quality < 50)
                 {
-                    if (t.Name != AgedBrie)
-                    {
-                        if (t.Name != BackstagePassesToATafkal80EtcConcert)
-                        {
-                            if (t.Quality > 0)
-                            {
-                                t.Quality = t.Quality - 1;
-                            }
-                        }
-                        else
-                        {
-                            t.Quality = t.Quality - t.Quality;
-                        }
-                    }
-                    else
-                    {
-                        if (t.Quality < 50)
-                        {
-                            t.Quality = t.Quality + 1;
-                        }
-                    }
+                    t.Quality = t.Quality + 1;
                 }
             }
         }
